656eb66288f1a8afdb0d483d5aa8e4d8617383b6
Krishnadeshpande2907 <kd721921@gmail.com>
2025-07-02 08:31:00 +0530

    files without errors for now

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,63 +1,64 @@
+import { Redirect, Tabs } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { useAuth } from '../../providers/auth-provider';
 
-import { Tabs } from "expo-router"
-import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
-
-function TabBarIcon(props:{
-    name: React.ComponentProps<typeof FontAwesome>['name'];
-    color: string;
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
 }) {
-    return (
-        <FontAwesome
-            size={24}
-            {...props}
-            color="#1BC464"
-        />
-    );
+  return <FontAwesome size={24} {...props} style={{ color: '#1BC464' }} />;
 }
 
 const TabsLayout = () => {
-    return (
-        <SafeAreaView edges={['top']} style={styles.safeArea}>
-            <Tabs screenOptions={{
-                tabBarActiveTintColor: '#1BC464',
-                tabBarInactiveTintColor: 'gray',
-                tabBarLabelStyle: { fontSize: 16 },
-                tabBarStyle: {
-                    borderTopLeftRadius: 20,
-                    borderTopRightRadius: 20,
-                    paddingTop: 10,
-                },
-                headerShown: false,
-            }}>
-                <Tabs.Screen
-                    name="index"
-                    options={{
-                        title: 'Shop',
-                        tabBarIcon(props) {
-                            return <TabBarIcon {...props} name="shopping-cart" />;
-                        },
-                    }}
-                />
-                <Tabs.Screen
-                    name="orders"
-                    options={{
-                        title: 'Orders',
-                        tabBarIcon(props) {
-                            return <TabBarIcon {...props} name= "book" />;
-                        },
-                    }}
-                />
-            </Tabs>
-        </SafeAreaView>
-    );
+  const { session, mounting } = useAuth();
+
+  if (mounting) return <ActivityIndicator />;
+  if (!session) return <Redirect href='/auth' />;
+
+  return (
+    <SafeAreaView edges={['top']} style={styles.safeArea}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#1BC464',
+          tabBarInactiveTintColor: 'gray',
+          tabBarLabelStyle: { fontSize: 16 },
+          tabBarStyle: {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingTop: 10,
+          },
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen
+          name='index'
+          options={{
+            title: 'shop',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name='shopping-cart' />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name='orders'
+          options={{
+            title: 'Orders',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name='book' />;
+            },
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
+  );
 };
 
 export default TabsLayout;
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1,
-    },
+  safeArea: {
+    flex: 1,
+  },
 });
